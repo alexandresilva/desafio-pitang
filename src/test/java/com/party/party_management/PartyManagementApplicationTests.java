71b1aae71ccdbb20da8f5a0b5eb461e93e31256f
Alexandre Silva <alexandre.silva.8@ebserh.gov.br>
2025-05-17 10:47:12 -0300

    Commit inicial

---
+++ src/test/java/com/party/party_management/PartyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.party.party_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
