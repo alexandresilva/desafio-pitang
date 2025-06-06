bcc1deec142228ae23b4e4a9c8da1d8bc5d7a3fa
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2025-06-05 22:54:48 -0300

    Adiciona script de inserção de usuário (V3)

---
+++ src/main/resources/db/migration/V3__insert_novo_usuario.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (email, username, "password", full_name, role_id, created_at, tags)
+VALUES (
+  'artur@email.com',
+  'artur.pitang',
+  '$2a$12$NLSzYRPBrIgqCtUUt2y4suZ3Y8k8ln6evQKeZaVgRcbV8UjHPp3YO',
+  'Usuário Novo',
+  2,
+  CURRENT_TIMESTAMP,
+  null
+);
