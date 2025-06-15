d256b38ce30582633b72f58f0787feedb7e90469
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2025-06-15 00:15:16 -0300

    implentação do CORS para acesso das rotas via Angular

--- src/main/java/com/party/party_management/config/WebConfig.java
+++ src/main/java/com/party/party_management/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.text.SimpleDateFormat;
@@ -23,5 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .serializationInclusion(JsonInclude.Include.NON_NULL);
 
         converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
